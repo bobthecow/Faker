398aaf9e7e787a6ba475974466e026d6ff793a97
Justin Hileman <justin@justinhileman.info>
2011-07-31 23:22:54 -0400

    Make helper methods in \Faker\Faker protected like they shoulda' been from the beginning.

--- src/Faker/Faker.php
+++ src/Faker/Faker.php
@@ -18,22 +18,22 @@ namespace Faker;
  */
 abstract class Faker
 {
-    public static function numerify($numberString)
+    protected static function numerify($numberString)
     {
         return preg_replace_callback("/#/", function() { return rand(0, 9); }, $numberString);
     }
 
-    public static function letterify($letterString)
+    protected static function letterify($letterString)
     {
         return preg_replace_callback("/\?/", function() { return chr(rand(97,122)); }, $letterString);
     }
 
-    public static function bothify($string)
+    protected static function bothify($string)
     {
         return self::letterify(self::numerify($string));
     }
 
-    public static function pickOne(array $options)
+    protected static function pickOne(array $options)
     {
         return $options[array_rand($options)];
     }
